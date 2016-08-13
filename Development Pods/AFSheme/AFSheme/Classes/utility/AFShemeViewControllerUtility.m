7a891683349beee390bf1153137261ff88f1c730
Aoi Fukuoka <aoponaopon85746@gmail.com>
2016-08-13 16:55:02 +0900

    Fix date.

--- Development Pods/AFSheme/AFSheme/Classes/utility/AFShemeViewControllerUtility.m
+++ Development Pods/AFSheme/AFSheme/Classes/utility/AFShemeViewControllerUtility.m
@@ -2,7 +2,7 @@
 //  AFShemeViewControllerUtility.m
 //  AFSheme
 //
-//  Created by aoponaopon on 2016/08/06.
+//  Created by aoponaopon on 2016/08/13.
 //  Copyright © 2016年 Aoi Fukuoka. All rights reserved.
 //
 
